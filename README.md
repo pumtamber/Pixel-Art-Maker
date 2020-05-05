200ff804d9321d28fd13331eba72cb8333d73a0d
Paramjit Kaur <pumtamber@gmail.com>
2020-05-05 12:57:20 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Pixel Art Maker Project for Udacity
+
+## Project Overview
+This project was a part of Udacity's [Introduction to Programming]((https://www.udacity.com/course/intro-to-programming-nanodegree--nd000).)
+
+In this project, I have use front-end programming skills.
+
+## Why this Project
+
+Because, this project helpped me understand how to use DOM,  using methods of the document object.
+
+Also, I have learned declaring functions and attaching them to DOM objects as event listeners. 
+
+To learn more about this program, you can follow this [link](https://www.udacity.com/course/intro-to-programming-nanodegree--nd000).
+
+
+## What Have I Learned
+
+Through this project, I have:
+
+- Learn how to submit projects.
+- Writing nested loops like For loop, While loop and Foreach loop.
+- Using loop variables.
+- DOM using methods of the document object.
+- variables with const and letariables with const and let.
---
+++ designs.js
@@ -0,0 +1,54 @@
+// Select color input
+const color = document.getElementById("colorPicker");
+// Select size input
+const height = document.getElementById('inputHeight');
+const width = document.getElementById('inputWidth');
+const sizePicker = document.getElementById("sizePicker");
+//Selecting canvas
+const canvas = document.getElementById('pixelCanvas');
+
+// When size is submitted by the user, call makeGrid()
+// make grid
+function makeGrid() {
+
+    for (let index = 0; index < height.value; index++) {
+        const element = canvas.insertRow(index);
+        
+        for (let r = 0; r < width.value; r++) {
+            const cell = element.insertCell(r);
+                cell.addEventListener("click", fillSquare);
+        } 
+    }
+}
+// event listener
+
+color.addEventListener("click", function() {});
+
+// form grid
+sizePicker.onsubmit = function(event) {
+
+    event.preventDefault();
+
+    clearGrid();
+
+    makeGrid();
+
+};
+
+// clear grid
+function clearGrid(){
+    while (canvas.firstChild) {
+
+        canvas.removeChild(canvas.firstChild);
+
+    }
+
+}
+// filling the grid with selected colors
+function fillSquare(){
+
+    this.setAttribute("style", 'background-color: ${color.value}');
+
+
+}
+
